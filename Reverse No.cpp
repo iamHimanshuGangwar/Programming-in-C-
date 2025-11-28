7b5da75c688bc68ec89e02e41ec9df5f291699c8
Himanshu Gangwar <your_verified_email@example.com>
2025-11-28 16:05:29 +0530

    first commit

---
+++ Reverse No.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, rev = 0, digit;
+    cin >> n;
+
+    while (n > 0)
+    {
+        digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
+
+    cout << rev;
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# How to run above code 
+g++ "file name" -o reverse
+./reverse
