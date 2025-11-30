2216242c773ab025ff0c16887de7f7415e54ad0f
Himanshu Gangwar <your_verified_email@example.com>
2025-11-30 21:54:20 +0530

    Daily update: Sun Nov 30 21:54:20 IST 2025

---
+++ palindrome no/palindrome.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num, rev = 0, original;
+    cin >> num;
+    original = num;
+
+    while (num > 0) {
+        int digit = num % 10;
+        rev = rev * 10 + digit;
+        num /= 10;
+    }
+
+    if (original == rev) {
+        cout << "Palindrome";
+    } else {
+        cout << "Not a Palindrome";
+    }
+
+    return 0;
+}
